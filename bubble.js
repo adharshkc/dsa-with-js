8df2d1174564bc774bb02109c2a2153258eeb883
adharshkc <adharshkc2017@gmail.com>
2024-04-16 10:51:37 +0530

    selection sort

--- bubble.js
+++ bubble.js
@@ -3,7 +3,6 @@ function bubbleSort(arr) {
     for (let i = 0; i < len; i++) {
         for (let j = 0; j < len - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap elements
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -13,7 +12,6 @@ function bubbleSort(arr) {
     return arr;
 }
 
-// Example usage:
 let myArray = [64, 34, 25, 12, 22, 11, 90];
 console.log("Original Array: " + myArray);
 console.log("Sorted Array: " + bubbleSort(myArray));
